08a2b6a541647cfaae7440ea42aef55ccc6645ab
liuyuan <709503583@qq.com>
2021-11-22 14:52:28 +0800

    Removes the provider setting and improves the Readme

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/ecs-ecs-gpu-instance"
-}
 locals {
   // This type of instance contains the following instance type families
   instance_type_families = ["ecs.gn6v", "ecs.gn5", "ecs.gn6i", "ecs.gn5"]
