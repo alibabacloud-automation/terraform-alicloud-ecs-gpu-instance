39d86f1ebe4dda0813cdecb47726d0333634896d
shanye <shanye.xzq@alibaba-inc.com>
2024-08-07 20:04:58 +0800

    CHANGELOG

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -14,28 +14,23 @@ provider "alicloud" {
 }
 
 #############################################################
-# Data sources to get VPC, vswitch and default security group details
+# create VPC, vswitch and security group
 #############################################################
 
-data "alicloud_vpcs" "default" {
-  is_default = true
-}
-
-data "alicloud_security_groups" "default" {
-  name_regex = "default"
-  vpc_id     = data.alicloud_vpcs.default.ids.0
-}
-
-data "alicloud_vswitches" "default" {
-  is_default = true
-  zone_id    = var.zone_id
+resource "alicloud_vpc" "default" {
+  vpc_name   = "tf_module"
+  cidr_block = "172.16.0.0/12"
 }
 
 resource "alicloud_vswitch" "default" {
-  count             = length(data.alicloud_vswitches.default.ids) > 0 ? 0 : 1
-  availability_zone = var.zone_id
-  vpc_id            = data.alicloud_vpcs.default.ids.0
-  cidr_block        = cidrsubnet(data.alicloud_vpcs.default.vpcs.0.cidr_block, 5, 11)
+  vpc_id     = alicloud_vpc.default.id
+  cidr_block = "172.16.0.0/21"
+  zone_id    = var.zone_id
+}
+
+resource "alicloud_security_group" "default" {
+  name   = "default"
+  vpc_id = alicloud_vpc.default.id
 }
 
 
@@ -48,8 +43,8 @@ module "ecs_instance" {
   instance_type_family        = "ecs.gn6v"
   instance_name               = "TF-ECS-Instance"
   password                    = "YourPassoword123"
-  vswitch_id                  = length(data.alicloud_vswitches.default.ids) > 0 ? data.alicloud_vswitches.default.ids.0 : concat(alicloud_vswitch.default.*.id, [""])[0]
-  security_group_ids          = data.alicloud_security_groups.default.ids
+  vswitch_id                  = alicloud_vswitch.default.id
+  security_group_ids          = [alicloud_security_group.default.id]
   associate_public_ip_address = true
   internet_max_bandwidth_out  = 10
 
---
+++ tfversion.md
@@ -0,0 +1,5 @@
+
+### Versions
+
+Terraform v1.7.5
+on darwin_arm64
